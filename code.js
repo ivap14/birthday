ceb6bf2777703a98c2c4203bea56af32cd4b0880
Iva Palova <iva.palova@consilia-brno.cz>
2024-04-07 17:22:14 +0200

    adding new page

---
+++ code.js
@@ -0,0 +1,50 @@
+document.getElementById('generateButton').addEventListener('click', function() {
+  // Redirect to the index.html page
+  window.location.href = 'index.html';
+});
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  const startButton = document.getElementById('startButton');
+  
+  startButton.addEventListener('click', () => {
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    const audioContext = new AudioContext();
+    
+    navigator.mediaDevices.getUserMedia({ audio: true })
+      .then((stream) => {
+        const microphone = audioContext.createMediaStreamSource(stream);
+        const analyser = audioContext.createAnalyser();
+        
+        analyser.fftSize = 2048;
+        const bufferLength = analyser.frequencyBinCount;
+        const dataArray = new Uint8Array(bufferLength);
+        
+        microphone.connect(analyser);
+        
+        // Start analyzing audio input
+        const detectBlow = () => {
+          analyser.getByteTimeDomainData(dataArray);
+
+          const maxAmplitude = Math.max(...dataArray);
+          if (maxAmplitude > 200) {
+            console.log('Blow detected!');
+            extinguishCandle();
+          }
+          requestAnimationFrame(detectBlow);
+        };
+        detectBlow();
+      })
+      .catch((error) => {
+        console.error('Error accessing microphone:', error);
+      });
+  });
+});
+
+function extinguishCandle() {
+  const flame = document.querySelector('.flame');
+  if (flame) {
+    flame.style.display = 'none';
+  }
+}
